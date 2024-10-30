90c78024e07b936c735412bc237b99e25a842271
Mateus Robers <robersmateus@gmail.com>
2024-10-30 11:10:17 -0300

    wip: adicionando o componente login

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  @ViewChild('container') container!: ElementRef;
 
+  constructor(private renderer: Renderer2) {}
+
+  activateRegister() {
+    this.renderer.addClass(this.container.nativeElement, 'active');
+  }
+
+  activateLogin() {
+    this.renderer.removeClass(this.container.nativeElement, 'active');
+  }
 }
