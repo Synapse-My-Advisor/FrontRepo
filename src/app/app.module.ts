21bffebf488d95d0d6db4e0f9fb7ed0635634324
Carlos Viel <charlievmusic@gmail.com>
2024-11-01 17:13:49 -0300

    Tela de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AdvisorComponent } from './components/advisor/advisor.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    AdvisorComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
